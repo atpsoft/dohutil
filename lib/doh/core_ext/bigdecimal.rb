4b7c067094a3e0cee38c97d807e3d36f83f50066
Makani Mason <gitauthor@pqmland.com>
2012-02-21 10:50:23 -0700

    don't re-define to_s or inspect; getting conflicts with other libraries

--- lib/doh/core_ext/bigdecimal.rb
+++ lib/doh/core_ext/bigdecimal.rb
@@ -1,22 +1,15 @@
 require 'bigdecimal'
+require 'bigdecimal/util'
 
 class Integer
+  # this is being added in 1.9.3, so remove it once that is the required version
   def to_d
     to_s.to_d
   end
 end
 
 class BigDecimal
-  alias :_original_to_s :to_s
-  def to_s(format = 'F')
-    _original_to_s(format)
-  end
-
-  undef inspect
-  def inspect
-    to_s
-  end
-
+  # this is being added in 1.9.3, so remove it once that is the required version
   def to_d
     self
   end
@@ -25,7 +18,7 @@ class BigDecimal
     raise ArgumentError.new("digits_after_decimal must be > 0") unless digits_after_decimal > 0
     return '0.' + ('0' * digits_after_decimal) if nan? || infinite?
 
-    retval = truncate(digits_after_decimal).to_s
+    retval = truncate(digits_after_decimal).to_s('F')
     digits_needed = retval.index('.') + digits_after_decimal + 1 - retval.size
     retval += ('0' * digits_needed) if digits_needed > 0
 
