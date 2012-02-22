6dbb9e4b0c84e71cb620f6dca0727b0da63a4cdf
Makani Mason <gitauthor@pqmland.com>
2012-02-22 14:59:44 -0700

    add a few more things to make DateTime / Time more interchangeable

--- lib/doh/core_ext/datewithtime.rb
+++ lib/doh/core_ext/datewithtime.rb
@@ -1,12 +1,22 @@
 require 'date'
 require 'time'
 
-# these are a set of methods to make DateTime objects more interchangeable with Time objects
-# they are not comprehensive, nor exact - for example, this utc method does not modify the receiver, whereas the Time equivalent does
+# these are sets of methods to make DateTime & Time objects more interchangeable
+# they are not comprehensive, nor exact - for example, this DateTime utc method does not modify the receiver, whereas the Time utc method does
 # they simply provide good enough compatibility for what I've encountered so far
 module MakeDateTimeLikeTime
   def utc
     new_offset
+  end
+end
+
+module MakeTimeLikeDateTime
+  def next_second(n = 1)
+    self + n
+  end
+
+  def prev_second(n = 1)
+    self + n
   end
 end
 
@@ -23,16 +33,20 @@ class DateTime
     strftime('%F %X')
   end
 
-  def add_seconds(seconds)
-    self + Rational(seconds, DOHRUBY_SECONDS_IN_DAY)
+  def next_second(n = 1)
+    self + (SECONDS_IN_DAY * n)
   end
 
-  def sub_seconds(seconds)
-    add_seconds(-seconds)
+  def prev_second(n = 1)
+    next_second(-n)
   end
 
   # subtract another DateTime object, return difference in seconds
   def sub_dt(other)
     ((self - other) * DOHRUBY_SECONDS_IN_DAY).to_i
   end
+end
+
+class Time
+  include MakeTimeLikeDateTime
 end
