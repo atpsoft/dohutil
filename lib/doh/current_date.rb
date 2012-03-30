0b085e75ccb58f7063a9624a8296fe09b000bc9c
Makani Mason <gitauthor@pqmland.com>
2012-03-30 11:35:21 -0600

    misc refactoring

--- lib/doh/current_date.rb
+++ lib/doh/current_date.rb
@@ -2,38 +2,36 @@ require 'doh/core_ext/datewithtime'
 
 module Doh
 
-@@current_date_objs = []
+@current_date_stack = []
 
 def self.current_date(dflt = Date.today)
-  return dflt if @@current_date_objs.empty?
-  @@current_date_objs.last.to_date
+  return dflt if @current_date_stack.empty?
+  @current_date_stack.last.to_date
 end
 
 def self.current_datetime(dflt = DateTime.zow)
-  return dflt if @@current_date_objs.empty?
-  cdo = @@current_date_objs.last
+  return dflt if @current_date_stack.empty?
+  cdo = @current_date_stack.last
   return cdo if cdo.respond_to?(:hour)
   DateTime.new(cdo.year, cdo.month, cdo.day, dflt.hour, dflt.min, dflt.sec, dflt.zone)
 end
 
 def self.push_current_date(date_obj)
-  @@current_date_objs.push(date_obj)
+  @current_date_stack.push(date_obj)
 end
 
 def self.pop_current_date
-  @@current_date_objs.pop
+  @current_date_stack.pop
 end
 
 def self.clear_current_date
-  @@current_date_objs.clear
+  @current_date_stack.clear
 end
 
-def self.date_as(date_obj, &block)
+def self.date_as(date_obj)
   push_current_date(date_obj)
   begin
-    retval = block.call
-  rescue
-    raise
+    retval = yield
   ensure
     pop_current_date
   end
