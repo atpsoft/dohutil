2cdd3a186d712e87cdc1c9884815091150727482
Makani Mason <gitauthor@pqmland.com>
2012-04-02 14:14:31 -0600

    fix warnings

--- lib/doh/core_ext/inspect.rb
+++ lib/doh/core_ext/inspect.rb
@@ -2,24 +2,28 @@ require 'date'
 require 'bigdecimal'
 
 class Date
+  undef inspect
   def inspect
     "#<Date:#{strftime('%F')}>"
   end
 end
 
 class DateTime
+  undef inspect
   def inspect
     "#<DateTime:#{strftime('%F %X %z')}>"
   end
 end
 
 class Time
+  undef inspect
   def inspect
     "#<Time:#{strftime('%F %X %z')}>"
   end
 end
 
 class BigDecimal
+  undef inspect
   def inspect
     "#<BigDecimal:#{to_s('F')}>"
   end
--- lib/doh/core_ext/string.rb
+++ lib/doh/core_ext/string.rb
@@ -2,7 +2,7 @@ class String
   def firstn(limit)
     return nil if limit < 0
     return '' if limit == 0
-    slice(Range.new(0, limit -1))
+    slice(Range.new(0, limit - 1))
   end
 
   def lastn(limit)
