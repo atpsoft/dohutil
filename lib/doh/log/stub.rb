636b7db1cc79502b070448d74ddcdb5684e2a958
Makani Mason <gitauthor@pqmland.com>
2012-04-03 10:11:58 -0600

    tweak with DohLog stub to support disable/enable

--- lib/doh/log/stub.rb
+++ lib/doh/log/stub.rb
@@ -1,14 +1,25 @@
+module DohLog
+  class StubInterface
+    def self.debug(msg); end
+    def self.info(msg); end
+    def self.warn(msg); end
+    def self.error(msg, exception = nil); end
+    def self.fatal(msg, exception = nil); end
+  end
+end
+
+class Module
+  unless method_defined?(:dohlog)
+    def dohlog
+      DohLog::StubInterface
+    end
+  end
+end
+
 class Object
   unless method_defined?(:dohlog)
-    class DohlogStubInterface
-      def self.debug(msg); end
-      def self.info(msg); end
-      def self.warn(msg); end
-      def self.error(msg, exception = nil); end
-      def self.fatal(msg, exception = nil); end
-    end
     def dohlog
-      DohlogStubInterface
+      DohLog::StubInterface
     end
   end
 end
