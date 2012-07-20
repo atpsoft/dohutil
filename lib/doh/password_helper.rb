8976b94785bae2dddf448c0409159b07e4fcd8ba
Kem Mason <bulbous@gmail.com>
2012-07-21 10:23:09 +1200

    add password_helper / thin wrapper around highline gem

---
+++ lib/doh/password_helper.rb
@@ -0,0 +1,14 @@
+require 'highline'
+
+#thin wrapper around HighLine, in case we decide to use something else in the future
+
+module Doh
+class PasswordHelper
+  @@highline = HighLine.new
+
+  def self.prompt_password(prompt = "Password: ")
+    @@highline.ask(prompt) {|q| q.echo = false}
+  end
+
+end
+end
