c5c653d99ac643eaea1751a7561450a931f797e8
Kem Mason <bulbous@gmail.com>
2013-06-12 14:48:26 +1200

    remove zow, add DateTime.local support

--- lib/dohutil/core_ext/date.rb
+++ lib/dohutil/core_ext/date.rb
@@ -1,4 +1,5 @@
 require 'date'
+require 'time'
 
 class Date
   def self.days_in_month(year, month)
@@ -11,5 +12,15 @@ class Date
 
   def make_datetime(hour = 0, minute = 0, second = 0, offset = 0)
     DateTime.new(year, month, mday, hour, minute, second, offset)
+  end
+
+  def make_local_datetime(hour = 0, minute = 0, second = 0)
+    DateTime.local(year, month, mday, hour, minute, second)
+  end
+end
+
+class DateTime
+  def self.local(year, month, day, hour = 0, minute = 0, second = 0)
+    Time.local(year, month, day, hour, minute, second).to_datetime
   end
 end
