b09c59418643fc61efcf86176482e7b973135a9e
Makani Mason <gitauthor@pqmland.com>
2013-12-06 15:53:10 -0700

    add a unit test for sub_dt

--- test/core_ext/datewithtime.dt.rb
+++ test/core_ext/datewithtime.dt.rb
@@ -7,6 +7,10 @@ class Test_core_ext_datewithtime < DohTest::TestGroup
     assert_equal(DateTime.new(2009, 2, 10, 15, 30, 3), DateTime.new(2009, 2, 10, 15, 30, 2).next_second(1))
     assert_equal(Time.new(2009, 2, 10, 15, 30, 3), Time.new(2009, 2, 10, 15, 30, 2).next_second(1))
   end
+
+  def test_datetime_diff
+    assert_equal(61, DateTime.new(2013,4,5,8,1,1).sub_dt(DateTime.new(2013,4,5,8,0,0)))
+  end
 end
 
 end
